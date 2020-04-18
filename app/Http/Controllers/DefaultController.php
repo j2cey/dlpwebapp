aecae59c4ebe033f1321b520140e5411fb11f9bc
vagrant <vagrant@vagrant.vm>
2020-04-18 18:44:21 +0000

    Fixe message heure 24

--- app/Http/Controllers/DefaultController.php
+++ app/Http/Controllers/DefaultController.php
@@ -20,11 +20,10 @@ class DefaultController extends Controller
       Carbon::setLocale('fr');
 
       $date_debut = Carbon::now();
+      $date_limite = Carbon::create($date_debut->year, $date_debut->month, $date_debut->day, 19, 30, 00);
       $date_fin = Carbon::now();
-      $msg_autorisation = "";
 
-      //$request_parsed = -1;
-      //$request_msg = "";
+      $msg_autorisation = "";
 
       // 1. Enregistrement de la requeete
       $curr_requete = Requete::create([
@@ -44,38 +43,29 @@ class DefaultController extends Controller
       }
       $curr_requete->demandeur_id = $demandeur->id;
 
-      //dd($curr_requete, $demandeur);
-
       // 3. Analyse de la Requete
       if ($reqtype == '1') {
         $curr_requete->req_code = 1;
-
-        // date('d-m-Y H:i:s'
-        // $date->toTimeString(); // Will output 14:15:16.
-        // $date->toDateString(); // Will output 1975-12-25.
-
         $validite_heure = 3;
-        $date_fin->addHours(3); //date('Y-m-d H:i:s', strtotime('now +3 hour'));
-        $curr_requete->msg = "votre demande de deplacement alimentaire du " . $date_debut->translatedFormat('jS F Y \\a h:i') . " au " . $date_fin->translatedFormat('jS F Y \\a h:i') . " a ete validee";
-        $msg_autorisation = "vous avez une autorisation de deplacement alimentaire du " . $date_debut->translatedFormat('jS F Y \\a h:i') . " au " . $date_fin->translatedFormat('jS F Y \\a h:i');
+
+        $curr_requete->msg = "votre demande de déplacement alimentaire du " . $date_debut->translatedFormat('jS F Y \\a H:i');
+        $msg_autorisation = "vous avez une autorisation de déplacement alimentaire du " . $date_debut->translatedFormat('jS F Y \\a H:i');
 
         $ecart_autorisation_jrs = 3;
       } elseif($reqtype == '2') {
         $curr_requete->req_code = 2;
 
         $validite_heure = 2;
-        $date_fin->addHours(3);
-        $curr_requete->msg = "votre demande de deplacement de sante du " . $date_debut->translatedFormat('jS F Y \\a h:i') . " au " . $date_fin->translatedFormat('jS F Y \\a h:i') . "a ete validee";
-        $msg_autorisation = "vous avez une autorisation de deplacement de sante du " . $date_debut->translatedFormat('jS F Y \\a h:i') . " au " . $date_fin->translatedFormat('jS F Y \\a h:i');
+        $curr_requete->msg = "votre demande de déplacement de santé du " . $date_debut->translatedFormat('jS F Y \\a H:i');
+        $msg_autorisation = "vous avez une autorisation de déplacement de santé du " . $date_debut->translatedFormat('jS F Y \\a H:i');
 
         $ecart_autorisation_jrs = 2;
       } elseif($reqtype == '3') {
         $curr_requete->req_code = 3;
-        $curr_requete->msg = "Deplacement d urgence autorise";
+        $validite_heure = 2;
 
-        $date_fin->addHours(3);
-        $curr_requete->msg = "votre demande de deplacement d urgence du " . $date_debut->translatedFormat('jS F Y \\a h:i') . " au " . $date_fin->translatedFormat('jS F Y \\a h:i') . "a ete validee";
-        $msg_autorisation = "vous avez une autorisation de deplacement d urgence du " . $date_debut->translatedFormat('jS F Y \\a h:i') . " au " . $date_fin->translatedFormat('jS F Y \\a h:i');
+        $curr_requete->msg = "votre demande de déplacement d urgence du " . $date_debut->translatedFormat('jS F Y \\a H:i');
+        $msg_autorisation = "vous avez une autorisation de déplacement d urgence du " . $date_debut->translatedFormat('jS F Y \\a H:i');
 
         $validite_heure = 24;
         $ecart_autorisation_jrs = 0;
@@ -96,18 +86,39 @@ class DefaultController extends Controller
       if ($curr_requete->req_code > -1 && $curr_requete->req_code < 4) {
         $autorisation_en_cours = $demandeur->autorisationEnCours();
         if (is_null($autorisation_en_cours)) {
-            // On donne une nouvelle autorisation au Demandeur
-            $new_autorisation = Autorisation::create([
-              'demandeur_id' => $demandeur->id,
-              'requete_id' => $curr_requete->id,
-              'code' => $curr_requete->req_code,
-              'msg' => $msg_autorisation,
-              'is_active' => true,
-              'date_debut' => $date_debut,
-              'date_fin' => $date_fin,
-            ]);
 
-            $demandeur->is_requesting = false;
+            $heures_restantes = $date_debut->diffInHours($date_limite);
+
+            if ($heures_restantes < 0) {
+                // Heure limite atteinte
+                $curr_requete->req_code = -3;
+                $demandeur->is_requesting = false;
+                $curr_requete->msg = "Désolé. Heure limite atteinte pour les demandes d autorisation";
+            } else {
+                if ($heures_restantes < $validite_heure) {
+                    // On assigne la date limite
+                    $date_fin = $date_limite;
+                } else {
+                    // On recupère la date de fin normale
+                    $date_fin->addHours($validite_heure);
+                }
+
+                $curr_requete->msg = $curr_requete->msg . " au " . $date_fin->translatedFormat('jS F Y \\a H:i') . " a ete validee";
+                $msg_autorisation = $msg_autorisation . " au " . $date_fin->translaj2tedFormat('jS F Y \\a H:i');
+
+                // On donne une nouvelle autorisation au Demandeur
+                $new_autorisation = Autorisation::create([
+                  'demandeur_id' => $demandeur->id,
+                  'requete_id' => $curr_requete->id,
+                  'code' => $curr_requete->req_code,
+                  'msg' => $msg_autorisation,
+                  'is_active' => true,
+                  'date_debut' => $date_debut,
+                  'date_fin' => $date_fin,
+                ]);
+
+                $demandeur->is_requesting = false;
+            }
         } else {
             // Le demandeur a deja une autorisation non echue
             $curr_requete->req_code = -2;
@@ -120,7 +131,7 @@ class DefaultController extends Controller
       $demandeur->save();
 
       return response()->json([
-             'message' => $curr_requete->msg
-         ]);
+        'message' => $curr_requete->msg
+      ]);
     }
 }
